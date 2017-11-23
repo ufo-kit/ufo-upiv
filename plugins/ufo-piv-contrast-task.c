7acdfb8fdad399d295def318dfb04bcfba44701c
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2017-11-23 15:30:23 +0100

    Clean up contrast task

--- plugins/ufo-piv-contrast-task.c
+++ plugins/ufo-piv-contrast-task.c
@@ -28,7 +28,6 @@
 
 struct _UfoPivContrastTaskPrivate {
     cl_kernel sigmoid_kernel;
-    cl_context context;
     gfloat c1;
     gfloat c2;
     gfloat c3;
@@ -64,16 +63,14 @@ ufo_piv_contrast_task_new (void)
 
 static void
 ufo_piv_contrast_task_setup (UfoTask *task,
-                       UfoResources *resources,
-                       GError **error)
+                             UfoResources *resources,
+                             GError **error)
 {
     UfoPivContrastTaskPrivate *priv;
 
     priv = UFO_PIV_CONTRAST_TASK_GET_PRIVATE (task);
     priv->sigmoid_kernel = ufo_resources_get_kernel (resources, "piv_contrast.cl", "sigmoid", error);
-    priv->context = ufo_resources_get_context (resources);
 
-    UFO_RESOURCES_CHECK_CLERR (clRetainContext (priv->context));
     if (priv->sigmoid_kernel != NULL)
         UFO_RESOURCES_CHECK_CLERR (clRetainKernel (priv->sigmoid_kernel));
 }
@@ -94,7 +91,7 @@ ufo_piv_contrast_task_get_num_inputs (UfoTask *task)
 
 static guint
 ufo_piv_contrast_task_get_num_dimensions (UfoTask *task,
-                                             guint input)
+                                          guint input)
 {
     return 2;
 }
@@ -105,58 +102,72 @@ ufo_piv_contrast_task_get_mode (UfoTask *task)
     return UFO_TASK_MODE_PROCESSOR | UFO_TASK_MODE_GPU;
 }
 
-static float array_min(float *data, int size) 
+static float
+array_min (float *data, int size)
 {
     float res = data[0];
-    for (int i = 1; i < size; i++) 
-    {
-        if (data[i] < res) res = data[i];
+
+    for (int i = 1; i < size; i++) {
+        if (data[i] < res)
+            res = data[i];
     }
+
     return res;
 }
 
-static float array_max(float *data, int size) 
+static float
+array_max (float *data, int size)
 {
     float res = data[0];
-    for (int i = 1; i < size; i++) 
-    {
-        if (data[i] > res) res = data[i];
+
+    for (int i = 1; i < size; i++) {
+        if (data[i] > res)
+            res = data[i];
     }
+
     return res;
 }
 
-static float array_mean(float *data, int size, float low, float high) 
+static float
+array_mean (float *data, int size, float low, float high)
 {
     float res = 0.0f;
     unsigned ct = 0;
+
     for (int i = 0; i < size; i++) {
         if (data[i] > low && data[i] < high) {
             res += data[i];
             ct++;
         }
     }
+
     return res/ct;
 }
 
-static float array_std(float *data, int size, float mean, float low, float high)
+static float
+array_std (float *data, int size, float mean, float low, float high)
 {
     float res = 0.0f;
     unsigned ct = 0;
+
     for (int i = 0; i < size; i++) {
         if (data[i] > low && data[i] < high) {
             res += (data[i]-mean) * (data[i]-mean);
             ct++;
         }
     }
+
     return sqrt(res/ct);
 }
 
-static int prepare_test(gfloat* test, gfloat* input, int offset_x, int offset_y, 
-                         int len_x, int len_y, int req_x, int req_y)
+static int
+prepare_test (gfloat* test, gfloat* input, int offset_x, int offset_y,
+              int len_x, int len_y, int req_x, int req_y)
 {
     int id, id_0;
     len_x= (len_x + offset_x) > req_x ? req_x - offset_x : len_x;
     len_y = (len_y + offset_y) > req_y ? req_y - offset_y : len_y;
+
     for (int i = 0; i < len_x; i++) {
         for (int j = 0; j < len_y; j++) {
             id = j * len_x + i;
@@ -164,32 +175,32 @@ static int prepare_test(gfloat* test, gfloat* input, int offset_x, int offset_y,
             test[id] = input[id_0];
         }
     }
-    return len_x*len_y;
+
+    return len_x * len_y;
 }
 
-static void approx_bg(gfloat* test, int size, float* m, float* s)
+static void
+approx_bg (gfloat* test, int size, float* m, float* s)
 {
     int ct;
     float low_cut, high_cut, std_old;
     float mean, std;
-    
+
     ct = 0;
     std = 0.0f;
-    low_cut = array_min(test, size);
-    high_cut = array_max(test, size);
+    low_cut = array_min (test, size);
+    high_cut = array_max (test, size);
 
-    while (ct < 10)
-    {
+    while (ct < 10) {
         ct++;
         std_old = std;
-        mean = array_mean(test, size, low_cut, high_cut);
-        std = array_std(test, size, mean, low_cut, high_cut);
-        //printf("mean = %2.8f std = %2.8f; min = %2.8f max = %2.8f\n", mean, std, minv, maxv);
+        mean = array_mean (test, size, low_cut, high_cut);
+        std = array_std (test, size, mean, low_cut, high_cut);
 
-        high_cut = MIN(high_cut, mean + 2*std);
-        low_cut = MAX(low_cut, mean - 2*std);
+        high_cut = MIN (high_cut, mean + 2*std);
+        low_cut = MAX (low_cut, mean - 2*std);
 
-        if (fabs(std - std_old) / std < 0.05)
+        if (fabs (std - std_old) / std < 0.05)
             break;
     }
 
@@ -199,9 +210,9 @@ static void approx_bg(gfloat* test, int size, float* m, float* s)
 
 static gboolean
 ufo_piv_contrast_task_process (UfoTask *task,
-                         UfoBuffer **inputs,
-                         UfoBuffer *output,
-                         UfoRequisition *requisition)
+                               UfoBuffer **inputs,
+                               UfoBuffer *output,
+                               UfoRequisition *requisition)
 {
     UfoPivContrastTaskPrivate *priv;
     UfoGpuNode *node;
@@ -209,26 +220,25 @@ ufo_piv_contrast_task_process (UfoTask *task,
     cl_command_queue *cmd_queue;
     cl_mem in_device;
     cl_mem out_device;
-    
+
     unsigned offset_x, offset_y, len_x, len_y, req_x, req_y;
     gfloat *in_mem, *test_mem;
     gfloat mean, std, high;
     int n;
- 
+
     req_x = requisition->dims[0];
     req_y = requisition->dims[1];
     offset_x = req_x / 4;
     offset_y = req_y / 4;
     len_x = req_x / 4;
     len_y = req_y / 4;
-   
+
     in_mem = ufo_buffer_get_host_array(inputs[0], NULL);
     test_mem = (gfloat*) g_malloc0 (len_x * len_y * sizeof(gfloat));
 
-    n = prepare_test(test_mem, in_mem, offset_x, offset_y, len_x, len_y, req_x, req_y);
-    approx_bg(test_mem, n, &mean, &std);
+    n = prepare_test (test_mem, in_mem, offset_x, offset_y, len_x, len_y, req_x, req_y);
+    approx_bg (test_mem, n, &mean, &std);
     g_free(test_mem);
-    //g_message("PivContrast: %f, %f", mean, std);
 
     gfloat *img = ufo_buffer_get_host_array(inputs[0], NULL);
     high = array_max(img, req_x*req_y);
@@ -258,9 +268,9 @@ ufo_piv_contrast_task_process (UfoTask *task,
 
 static void
 ufo_piv_contrast_task_set_property (GObject *object,
-                              guint property_id,
-                              const GValue *value,
-                              GParamSpec *pspec)
+                                    guint property_id,
+                                    const GValue *value,
+                                    GParamSpec *pspec)
 {
     UfoPivContrastTaskPrivate *priv = UFO_PIV_CONTRAST_TASK_GET_PRIVATE (object);
 
@@ -288,9 +298,9 @@ ufo_piv_contrast_task_set_property (GObject *object,
 
 static void
 ufo_piv_contrast_task_get_property (GObject *object,
-                              guint property_id,
-                              GValue *value,
-                              GParamSpec *pspec)
+                                    guint property_id,
+                                    GValue *value,
+                                    GParamSpec *pspec)
 {
     UfoPivContrastTaskPrivate *priv = UFO_PIV_CONTRAST_TASK_GET_PRIVATE (object);
 
@@ -320,7 +330,7 @@ static void
 ufo_piv_contrast_task_finalize (GObject *object)
 {
     UfoPivContrastTaskPrivate *priv = UFO_PIV_CONTRAST_TASK_GET_PRIVATE (object);
-    
+
     if (priv->sigmoid_kernel) {
         UFO_RESOURCES_CHECK_CLERR (clReleaseKernel (priv->sigmoid_kernel));
         priv->sigmoid_kernel = NULL;
@@ -349,40 +359,40 @@ ufo_piv_contrast_task_class_init (UfoPivContrastTaskClass *klass)
     oclass->get_property = ufo_piv_contrast_task_get_property;
     oclass->finalize = ufo_piv_contrast_task_finalize;
 
-    properties[PROP_C1] = g_param_spec_float(
-                            "c1",
-                            "lower cut",
-                            "lower cut relative to the background mean in unit of background standard deviation",
-                            -5.0f, 5.0f, 0.5f,
-                            G_PARAM_READWRITE);
+    properties[PROP_C1] =
+        g_param_spec_float ("c1",
+            "lower cut",
+            "lower cut relative to the background mean in unit of background standard deviation",
+            -5.0f, 5.0f, 0.5f,
+            G_PARAM_READWRITE);
 
-     properties[PROP_C2] = g_param_spec_float(
-                            "c2",
-                            "higher cut",
-                            "higher cut relative to the background mean in unit of background standard deviation",
-                            0.0f, 100.0f, 10.0f,
-                            G_PARAM_READWRITE);
+    properties[PROP_C2] =
+        g_param_spec_float ("c2",
+            "higher cut",
+            "higher cut relative to the background mean in unit of background standard deviation",
+            0.0f, 100.0f, 10.0f,
+            G_PARAM_READWRITE);
 
-      properties[PROP_C3] = g_param_spec_float(
-                            "c3",
-                            "sigmoid shift",
-                            "sigmoid shift relative to the background mean in unit of background standard deviation",
-                            0.0f, 20.0f, 3.0f,
-                            G_PARAM_READWRITE);
- 
-      properties[PROP_C4] = g_param_spec_float(
-                            "c4",
-                            "sigmoid width",
-                            "sigmoid shift in unit of background standard deviation",
-                            0.0f, 10.0f, 1.5f,
-                            G_PARAM_READWRITE);
+    properties[PROP_C3] =
+        g_param_spec_float ("c3",
+            "sigmoid shift",
+            "sigmoid shift relative to the background mean in unit of background standard deviation",
+            0.0f, 20.0f, 3.0f,
+            G_PARAM_READWRITE);
 
-      properties[PROP_GAMMA] = g_param_spec_float(
-                            "gamma",
-                            "intensity gamma",
-                            "intensity gamma",
-                            0.0f, 1.0f, 0.3f,
-                            G_PARAM_READWRITE);
+    properties[PROP_C4] =
+        g_param_spec_float ("c4",
+            "sigmoid width",
+            "sigmoid shift in unit of background standard deviation",
+            0.0f, 10.0f, 1.5f,
+            G_PARAM_READWRITE);
+
+    properties[PROP_GAMMA] =
+        g_param_spec_float ("gamma",
+            "intensity gamma",
+            "intensity gamma",
+            0.0f, 1.0f, 0.3f,
+            G_PARAM_READWRITE);
 
     for (guint i = PROP_0 + 1; i < N_PROPERTIES; i++)
         g_object_class_install_property (oclass, i, properties[i]);
